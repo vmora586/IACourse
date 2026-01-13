f7bd9e82e8aea73f27cb3adfa8de50ff264d4c80
Victor M <victormc.586@gmail.com>
2026-01-12 21:26:06 -0500

    Improve copy behaviour

--- packages/client/src/components/ui/ChatBot.tsx
+++ packages/client/src/components/ui/ChatBot.tsx
@@ -49,12 +49,21 @@ const ChatBot = () => {
         }
     };
 
+    const onCopy = (e: React.ClipboardEvent<HTMLParagraphElement>): void => {
+        const selection = window.getSelection()?.toString().trim();
+        if (selection) {
+            e.preventDefault();
+            e.clipboardData.setData('text/plain', selection);
+        }
+    };
+    
     return (
         <div>
             <div className="flex flex-col gap-3 mb-10">
                 {messages.map((message, index) => (
                     <p
                         key={index}
+                        onCopy={onCopy}
                         className={`px-3 py-1 rounded-xl ${message.role === 'user' ? 'bg-blue-600 text-white self-end' : 'bg-gray-100 text-black self-start'}`}
                     >
                         <ReactMarkdown>

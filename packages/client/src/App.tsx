245ce2a1f1e77b602b7e485166459715b23d949c
Victor M <victormc.586@gmail.com>
2026-01-12 22:37:28 -0500

    Improve look and feel

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,7 +3,7 @@ import ChatBot from './components/ui/ChatBot';
 
 function App() {
   return (
-    <div className='p-4'>
+    <div className="p-4 h-screen w-full" >
         <ChatBot/>
     </div>
   )
--- packages/client/src/components/ui/ChatBot.tsx
+++ packages/client/src/components/ui/ChatBot.tsx
@@ -22,10 +22,10 @@ const ChatBot = () => {
     const [messages, setMessages] = useState<Message[]>([]);
     const { register, handleSubmit, reset, formState } = useForm<FormData>();
     const [isBotTyping, setIsBotTyping] = useState(false);
-    const formRef = useRef<HTMLFormElement | null>(null);
+    const lastMessageRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
-        formRef.current?.scrollIntoView({ behavior: 'smooth' })
+        lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' })
     }, [messages]);
 
     const onSubmit = async ({ prompt }: FormData) => {
@@ -56,20 +56,21 @@ const ChatBot = () => {
             e.clipboardData.setData('text/plain', selection);
         }
     };
-    
+
     return (
-        <div>
-            <div className="flex flex-col gap-3 mb-10">
+        <div className="flex flex-col h-full">
+            <div className="flex flex-col flex-1 gap-3 mb-10 overflow-y-auto">
                 {messages.map((message, index) => (
-                    <p
+                    <div
                         key={index}
                         onCopy={onCopy}
                         className={`px-3 py-1 rounded-xl ${message.role === 'user' ? 'bg-blue-600 text-white self-end' : 'bg-gray-100 text-black self-start'}`}
+                        ref={index === messages.length - 1 ? lastMessageRef : null}
                     >
                         <ReactMarkdown>
                             {message.content}
                         </ReactMarkdown>
-                    </p>
+                    </div>
                 ))}
                 {isBotTyping && (
                     <div className="flex self-start gap-1 px-3 py-3 bg-gray-200 rounded-xl">
@@ -82,7 +83,6 @@ const ChatBot = () => {
             <form
                 onSubmit={handleSubmit(onSubmit)}
                 onKeyDown={onKeyDown}
-                ref={formRef}
                 className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl">
                 <textarea
                     {...register('prompt',
@@ -91,6 +91,7 @@ const ChatBot = () => {
                             validate: (data) => data.trim().length > 0
                         }
                     )}
+                    autoFocus
                     placeholder="Ask anything"
                     className="w-full border-0 focus:outline-0 resize-none"
                     maxLength={1000}
